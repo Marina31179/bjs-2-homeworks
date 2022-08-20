59f46ede35832eb7e142b0b3ceaae64e9ea804bd
marina <95930484+Marina31179@users.noreply.github.com>
2022-08-20 09:12:45 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,31 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = b**2 - 4*a*c;
+  if (d < 0) {
+    arr = [];
+  }
+  if (d === 0) {
+    arr = [-b/(2*a)];
+  }
+  else {
+    arr = [(-b + Math.sqrt(d) )/(2*a),(-b - Math.sqrt(d) )/(2*a)];
+  }
+
+  // код для задачи №1 писать здесь//
+  return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
+function calculateTotalMortgage(percent, contribution, amount, n) {
   let totalAmount;
+  let monthAmount;
+  n = totalAmount/monthAmount;
+  let S = amount;
+  let P = percent/12;
+  monthAmount = S*(P + (P/(((1 + P)**n) - 1)));
+  console.log('Вывод:', totalAmount)
 
-  // код для задачи №2 писать здесь
 
   return totalAmount;
 }
+
