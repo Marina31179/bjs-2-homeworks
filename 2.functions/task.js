f5c2b49548c0c87665cb78b40ab894a951ff6b23
marina <95930484+Marina31179@users.noreply.github.com>
2022-09-05 06:32:17 +0300

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,11 +1,24 @@
-// Задание 1
+// Задание 1//
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let sum = 0;
+  let min = - 100;
+  let max = 100;
+  let avg = sum / arr.length;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (arr[i] > min) {
+      min = arr[i];
+    }
+    if (arr[i + 1] > min) {
+      max = arr[i + 1];
+    }
+    return min, max, avg;
+  }
 
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
 }
+getArrayParams(97, 99, 10);
+
+
 
 // Задание 2
 function worker(arr) {
