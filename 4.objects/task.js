a94aab395a44e883d6ac9518cfde80e278e4386c
marina <95930484+Marina31179@users.noreply.github.com>
2022-10-17 06:35:30 +0300

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
-function Student(name, gender, age) {
-    // Ваш код
+let Student = function(name, gender, age, subject) {
+    this.name = name;
+    this.gender = (gender != 'female') ? 'male' : 'femail';
+    this.age = age;
+    this.subject = subject;
+    Student.prototype.setSubject = function (subjectName) {
+        subject = subjectName;
+        return subject;//ваш код
+    }
+
+    Student.prototype.addMark = function(mark) {
+        if (this.marks === undefined) {
+            this.marks = [mark]; // добавить первую оценку
+        } else {
+            this.marks.push(mark);
+
+        }
+    }
+    Student.prototype.addMarks(mark) = function() {
+        [marks].push(mark);
+        return [marks];
+    }
+
+    Student.prototype.getAverage(avg) = function() {
+        let sum = 0;
+        for (let i = 0; i < [mark].length; i++) {
+            sum += marks[i];
+        }
+        let avg = sum / [marks].length;
+        return { avg: avg };
+
+    }
+    console.log('age: ' + 'gender: ' + 'marks: ' + 'name: ' + 'subject: ');
+
+
 
 }
-
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
-
-// ваш код для остальных методов
