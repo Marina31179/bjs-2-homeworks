cfba4b2e1e23688fc0a32d4f49e03cf0c79754a5
marina <95930484+Marina31179@users.noreply.github.com>
2022-10-29 20:35:17 +0300

    forth commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,41 +1,40 @@
-let Student = function(name, gender, age) {
+function Student(name, gender, age) {
     this.name = name;
     this.gender = gender;
     this.age = age;
 }
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
 
-Student.prototype.setSubject =  function(subjectName) {
-    if (Student.subject === undefined) {
-        this.subjectName = Student.subject;
-    }
 }
-Student.prototype.addMark = function(mark) {
-    if (this.mark === undefined) {
-        this.mark = [mark];
+Student.prototype.addMark = function(marks) {
+    if (this.marks === undefined) {
+        this.marks = [marks];
     } else {
-        this.mark.push(mark);
+        this.marks.push(marks);
     }
 }
-Student.prototype.addMarks = function(...marks) {
-    this.marks.push[marks];
+Student.prototype.addMarks = function(marks) {
+    this.marks.push(...marks);
 }
 
 
 Student.prototype.getAverage = function() {
     let sum = 0;
-    let num;
-    for (let i = 0; i < [marks].length; i++) {
-        num = Number(marks[i]);
-        sum += num[i];
-        avg = sum / [marks].length;
+    for (let i = 0; i < this.marks.length; i++) {
+        sum += Number(this.marks[i]);
     }
+    return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function(reason) {
-    if (avg < 3) {
-        delete subject;
-        delete [marks];
-        Student.excluded = reason;
+    let sum = 0;
+    for (let i = 0; i < this.marks.length; i++) {
+        if (sum / this.marks.length < 3) {
+            delete this.subject;
+            delete this.marks;
+        }
+        this.reason = reason;
         return reason;
     }
 }
